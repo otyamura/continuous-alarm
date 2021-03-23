9a0715b209ac4e153fd7cd2062ab771ec28f96fd
otyamura <suzuki08suzuki@gmail.com>
2021-03-24 01:43:38 +0900

    UI調整

--- components/LocalNortification.tsx
+++ components/LocalNortification.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { Button } from 'react-native';
 import * as Notifications from 'expo-notifications';
 import { playSound } from './MakeSound';
 import { storage } from './storage';
